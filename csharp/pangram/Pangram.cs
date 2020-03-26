e8159750e52fac288eda33c1db3f28718fb63190
Craig A. McDonald <craig@mcdonald.codes>
2020-03-26 06:35:56 +0000

    Another exercise completed

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        var data = new List<char>("abcdefghijklmnopqrstuvwxyz");
+
+        foreach (char c in input.ToLower())
+        {
+            data.Remove(c);
+            if (data.Count == 0) { break; }
+        }
+
+        return data.Count == 0;
+    }
+}
