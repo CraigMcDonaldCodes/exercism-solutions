ae3300b9d620f5d029847ee57240e7afca83484b
Craig A. McDonald <craig@mcdonald.codes>
2020-03-27 21:19:08 +0000

    C# ErrorHandling exercise

---
+++ csharp/error-handling/ErrorHandling.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception("Error codes? We don't need no stinking error codes.");
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        if (Int32.TryParse(input, out int value))
+        {
+            return value;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        return Int32.TryParse(input, out result);
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        disposableObject.Dispose();
+        throw new Exception("Is this the exception you are looking for?");
+    }
+}
