36cd45c051bc08011059b8ca0ec9e12edd524d87
Craig A. McDonald <craig@mcdonald.codes>
2020-04-02 22:51:04 +0100

    C# secret handshake exercise

---
+++ csharp/secret-handshake/SecretHandshake.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+public static class SecretHandshake
+{
+    public static string[] Commands(int commandValue)
+    {
+        var result = new List<string>();
+
+        if ((commandValue & 1) == 1)   { result.Add("wink"); }
+        if ((commandValue & 2) == 2)   { result.Add("double blink"); }
+        if ((commandValue & 4) == 4)   { result.Add("close your eyes"); }
+        if ((commandValue & 8) == 8)   { result.Add("jump"); }
+        if ((commandValue & 16) == 16) { result.Reverse(); }
+
+        return result.ToArray();
+    }
+}
