5d2712d7d6793c88db39f3509792b8741dd66e3c
Craig A. McDonald <craig@mcdonald.codes>
2020-04-07 22:23:20 +0100

    C# ISBN verifier

---
+++ csharp/isbn-verifier/IsbnVerifier.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class IsbnVerifier
+{
+    public static bool IsValid(string number)
+    {
+        number = number.Replace("-", "");
+
+        return false;
+    }
+}
