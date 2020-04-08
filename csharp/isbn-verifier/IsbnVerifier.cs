c26aa5ad43bfb0c0d819b9e922f9bbbe72881750
Craig A. McDonald <craig@mcdonald.codes>
2020-04-08 23:05:17 +0100

    C# isbn verifier

--- csharp/isbn-verifier/IsbnVerifier.cs
+++ csharp/isbn-verifier/IsbnVerifier.cs
@@ -2,10 +2,25 @@
 
 public static class IsbnVerifier
 {
-    public static bool IsValid(string number)
+    public static bool IsValid(string isbnNumber)
     {
-        number = number.Replace("-", "");
+        if (string.IsNullOrEmpty(isbnNumber) ||
+            isbnNumber.Length != 13)
+        {
+                return false;
+        }
 
-        return false;
+        long result = 0;
+        int multiplier = 10;
+
+        foreach (var item in isbnNumber)
+        {
+            if (item == '-')             { continue; }
+            else if (item == 'X')        { result += 10; }
+            else if (char.IsDigit(item)) { result += multiplier * (long)item; }
+            multiplier--;
+        }
+
+        return result % 11 == 0;
     }
 }
