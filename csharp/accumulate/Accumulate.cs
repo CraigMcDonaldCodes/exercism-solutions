59a859d295d1e0d7d95ef7edfe024a50f71a5da3
Craig A. McDonald <craig@mcdonald.codes>
2020-04-26 12:19:25 +0100

    C# accumulate exercise cleanup

--- csharp/accumulate/Accumulate.cs
+++ csharp/accumulate/Accumulate.cs
@@ -5,8 +5,6 @@ public static class AccumulateExtensions
 {
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
     {
-        var newCollection = new List<U>();
-
         foreach (var item in collection)
         {
             yield return func(item);
