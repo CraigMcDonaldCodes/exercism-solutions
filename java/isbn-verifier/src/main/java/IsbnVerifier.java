a06d3e920f31f402b57db3b9d753608e2d6e62e9
Craig A. McDonald <craig@mcdonald.codes>
2020-06-29 19:51:26 +0100

    Java isbn problem completed

--- java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -1,12 +1,5 @@
 class IsbnVerifier {
 
-    public static void main(String... args) {
-
-        var verify = new IsbnVerifier();
-        System.out.println(verify.isValid("3-598-21507-X"));
-        System.out.println(verify.isValid("0-306-40615-2"));
-    }
-
     boolean isValid(String stringToVerify) {
 
         String isbn = stringToVerify.replace("-", "").toLowerCase();
@@ -15,17 +8,14 @@ class IsbnVerifier {
             return false;
         }
 
-        System.out.println(isbn);
-
         int sum = 0;
         int count = 10;
 
-        for(var s : isbn.toCharArray()) {
+        for (char value : isbn.toCharArray()) {
 
-            if (Character.isDigit(s)) {
-                sum += (s * count);
-            } else if (s == 'x' && count == 1) {
-                System.out.println("Was x");
+            if (Character.isDigit(value)) {
+                sum += Character.getNumericValue(value) * count;
+            } else if (value == 'x' && count == 1) {
                 sum += 10;
             } else {
                 sum = -1;
@@ -35,7 +25,6 @@ class IsbnVerifier {
             count--;
         }
 
-        System.out.printf("sum: %d, count: %d\n", sum, count);
         return sum % 11 == 0;
     }
 }
