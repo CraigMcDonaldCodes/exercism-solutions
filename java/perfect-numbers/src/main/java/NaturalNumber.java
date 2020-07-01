fbf3e59bbea6cf15933737e5d52caa768f6695fe
Craig A. McDonald <craig@mcdonald.codes>
2020-07-01 21:13:03 +0100

    Java perfect numbers exercise

---
+++ java/perfect-numbers/src/main/java/NaturalNumber.java
@@ -0,0 +1,12 @@
+class NaturalNumber {
+
+    private int number;
+
+    public NaturalNumber(int number) {
+        this.number = number;
+    }
+
+    public Classification getClassification() {
+        return Classification.PERFECT;
+    }
+}
