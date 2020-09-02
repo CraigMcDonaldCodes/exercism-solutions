5bec84ce74f4d6ea01c229104ac1fbc45c663548
Craig A. McDonald <craig@mcdonald.codes>
2020-09-02 21:53:40 +0100

    C# proverb problem completed

--- csharp/proverb/Proverb.cs
+++ csharp/proverb/Proverb.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class Proverb
 {
     public static string[] Recite(string[] subjects)
     {
-        return null;
+        var list = new List<string>();
+
+        if (subjects.Length > 0) {
+
+            for (int i = 1; i < subjects.Length; i++) {
+                list.Add($"For want of a {subjects[i - 1]} the {subjects[i]} was lost.");
+            }
+
+            list.Add($"And all for the want of a {subjects[0]}.");
+        }
+
+        return list.ToArray();
     }
 }
