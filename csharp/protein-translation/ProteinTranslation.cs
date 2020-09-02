6834929dc2df6a5650eff3ad58eaf2d48bda5eeb
Craig A. McDonald <craig@mcdonald.codes>
2020-09-02 22:31:42 +0100

    C# protein translation problem started

---
+++ csharp/protein-translation/ProteinTranslation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ProteinTranslation
+{
+    private static string STOP_VALUE = "STOP";
+    private static string SKIP_VALUE = "SKIP";
+    private static Dictionary<string, string> map = new Dictionary<string, string>() {
+        { "AUG", "Methionine" },
+        { "UUU", "Phenylalanine" },
+        { "UUC", "Phenylalanine" },
+        { "UUA", "Leucine" },
+        { "UUG", "Leucine" },
+        { "UCU", "Serine" },
+        { "UCC", "Serine" },
+        { "UCA", "Serine" },
+        { "UCG", "Serine" },
+        { "UAU", "Tyrosine" },
+        { "UAC", "Tyrosine" },
+        { "UGU", "Cysteine" },
+        { "UGC", "Cysteine" },
+        { "UGG", "Tryptophan" },
+        { "UAA", "STOP" },
+        { "UAG", "STOP" },
+        { "UGA", "STOP" }
+    };
+
+    public static string[] Proteins(string strand)
+    {
+        throw new NotImplementedException();
+    }
+}
