9c8caf1ea6a0d4b3d244d07c364365ebd2845827
Craig A. McDonald <craig@mcdonald.codes>
2020-09-04 23:49:39 +0100

    C# protein translation completed

--- csharp/protein-translation/ProteinTranslation.cs
+++ csharp/protein-translation/ProteinTranslation.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 
 public static class ProteinTranslation
 {
-    private static string STOP_VALUE = "STOP";
-    private static string SKIP_VALUE = "SKIP";
-    private static Dictionary<string, string> map = new Dictionary<string, string>() {
+    private static string CodonSplit = "(?<=\\G.{3})";
+    private static string StopValue = "STOP";
+    private static string SkipValue = "SKIP";
+    private static Dictionary<string, string> map = new Dictionary<string, string>()
+    {
         { "AUG", "Methionine" },
         { "UUU", "Phenylalanine" },
         { "UUC", "Phenylalanine" },
@@ -27,6 +30,27 @@ public static class ProteinTranslation
 
     public static string[] Proteins(string strand)
     {
-        throw new NotImplementedException();
+        string[] rnaParts = Regex.Split(strand, CodonSplit);
+        var result = new List<string>();
+
+        foreach(var entry in rnaParts)
+        {
+            string current = map.GetValueOrDefault(entry, SkipValue);
+
+            if (current == StopValue)
+            {
+                break;
+            }
+            else if (current == SkipValue)
+            {
+                continue;
+            }
+            else
+            {
+                result.Add(current);
+            }
+        }
+
+        return result.ToArray();
     }
 }
