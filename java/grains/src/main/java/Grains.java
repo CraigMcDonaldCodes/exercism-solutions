690159c7d9888578f3f265332b8259c151d6ea07
Craig A. McDonald <craig@mcdonald.codes>
2020-09-26 17:43:09 +0100

    Java grains problem completed

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -2,15 +2,30 @@ import java.math.BigInteger;
 
 class Grains {
 
+    private final int BASE = 2;
+    private final int MIN_SQUARES = 1;
     private final int MAX_SQUARES = 64;
+    private final String MSG_OUT_OF_RANGE = "square must be between 1 and 64";
 
     BigInteger grainsOnSquare(final int square) {
 
-        return new BigInteger(Double.toString(Math.pow(2, square - 1)));
+        boolean outOfRange = square < MIN_SQUARES
+                             ||
+                             square > MAX_SQUARES;
+
+        if (outOfRange) {
+            throw new IllegalArgumentException(MSG_OUT_OF_RANGE);
+        }
+
+        return BigInteger.valueOf(BASE)
+                         .pow(square - 1);
     }
 
     BigInteger grainsOnBoard() {
-        return grainsOnSquare(MAX_SQUARES);
+
+        return BigInteger.valueOf(BASE)
+                         .pow(MAX_SQUARES)
+                         .subtract(BigInteger.valueOf(1));
     }
 }
 
