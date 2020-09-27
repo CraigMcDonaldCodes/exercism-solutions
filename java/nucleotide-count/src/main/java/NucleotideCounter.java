b9e35aed31f2693f21813e9292bff5be7f204f79
Craig A. McDonald <craig@mcdonald.codes>
2020-09-27 12:33:03 +0100

    Java nucleotide count completed

--- java/nucleotide-count/src/main/java/NucleotideCounter.java
+++ java/nucleotide-count/src/main/java/NucleotideCounter.java
@@ -9,14 +9,30 @@ public class NucleotideCounter {
     private final Map<Character, Integer> data;
 
     public NucleotideCounter(final String input) {
+
         this.input = Objects.requireNonNull(input);
-
         data = prepDataStructure();
-
+        parseSuppliedData(input);
     }
 
     public Map<Character, Integer> nucleotideCounts() {
         return Collections.unmodifiableMap(data);
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    private void parseSuppliedData(final String input) {
+
+        for (var c : input.toCharArray()) {
+
+            if (data.containsKey(c)) {
+                data.put(c, data.get(c) + 1);
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     private Map<Character, Integer> prepDataStructure() {
