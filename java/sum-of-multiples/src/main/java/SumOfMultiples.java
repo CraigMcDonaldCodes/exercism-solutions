c2336443180c583f398dde8327c6598629c58ff3
Craig A. McDonald <craig@mcdonald.codes>
2020-09-28 14:27:01 +0100

    Java sum of multiples started

---
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -0,0 +1,10 @@
+class SumOfMultiples {
+
+    public SumOfMultiples(int number, int[] set) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    public int getSum() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
