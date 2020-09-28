9c50eb7fdc00704dcb4947dfa0e6b33d8487633b
Craig A. McDonald <craig@mcdonald.codes>
2020-09-28 13:56:30 +0100

    Java grade school problem completed

---
+++ java/grade-school/src/main/java/School.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.TreeMap;
+
+public class School {
+
+    private final int MIN_GRADE = 1;
+    private final int MAX_GRADE = 7;
+    private final String MSG_OUT_OF_RANGE = "Grade not in range.";
+    private final Map<Integer, Set<String>> grades;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    public School() {
+        grades = createDataStructure();
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    public void add(final String name, final int grade) {
+
+        if (inValidGrade(grade)) { throw new IllegalArgumentException(MSG_OUT_OF_RANGE); }
+
+        grades.get(grade).add(name);
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    public List<String> grade(final int grade) {
+
+        if (inValidGrade(grade)) { throw new IllegalArgumentException(MSG_OUT_OF_RANGE); }
+
+        return new ArrayList<String>(grades.get(grade));
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    public List<String> roster() {
+
+        var tmpRoster = new ArrayList<String>();
+
+        grades.values()
+              .forEach(g -> tmpRoster.addAll(g));
+
+        return tmpRoster;
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    private Map<Integer, Set<String>> createDataStructure() {
+
+        var tmpData = new TreeMap<Integer, Set<String>>();
+
+        for (int i = MIN_GRADE; i <= MAX_GRADE; i++) {
+            tmpData.put(i, new TreeSet<String>());
+        }
+
+        return tmpData;
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    private boolean inValidGrade(final int grade) {
+
+        return grade < MIN_GRADE
+               ||
+               grade > MAX_GRADE;
+    }
+}
