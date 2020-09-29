055cf3600928beaba7a6459c5945fa8ad2a8f7b7
Craig A. McDonald <craig@mcdonald.codes>
2020-09-29 12:56:24 +0100

    Java sum of multiples completed

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,10 +1,42 @@
 class SumOfMultiples {
 
+    private final int[] set;
+    private final int number;
+    private final int sum;
+
+    ////////////////////////////////////////////////////////////////////////////////
     public SumOfMultiples(int number, int[] set) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        this.number = number;
+        this.set = set;
+        sum = computeSum();
     }
 
+
+    ////////////////////////////////////////////////////////////////////////////////
     public int getSum() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return sum;
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    private int computeSum() {
+
+        // TODO: Bad variable names?
+        int total = 0;
+
+        for (int i = 1; i < number; i++) {
+
+            for (var s : set) {
+
+                // Don't allow division with zero
+                if (s != 0 && i % s == 0) {
+                    total += i;
+                    break;  // Only add the value once
+                }
+            }
+        }
+
+        return total;
     }
 }
