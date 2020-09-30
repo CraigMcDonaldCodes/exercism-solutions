5138932220b2f8fc35dac9b999605fa1e01c7bec
Craig A. McDonald <craig@mcdonald.codes>
2020-09-30 19:09:47 +0100

    Java flatten array completed

--- java/flatten-array/src/main/java/Flattener.java
+++ java/flatten-array/src/main/java/Flattener.java
@@ -3,7 +3,26 @@ import java.util.List;
 
 public class Flattener {
 
-   public List flatten() {
-       return null;
+    // Not sure this is the best way to solve this
+    // feels like I shouldn't be using `Object`
+   public List<Object> flatten(List<Object> data) {
+
+        var flatData = new ArrayList<Object>();
+
+        for (var item : data) {
+
+            if (item instanceof List) {
+
+                var subList = (List<Object>) item;
+                flatData.addAll(flatten(subList));
+            } else {
+
+                if (item != null) {
+                    flatData.add(item);
+                }
+            }
+        }
+
+       return flatData;
    }
 }
