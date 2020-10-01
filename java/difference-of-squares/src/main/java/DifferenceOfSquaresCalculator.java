596526b86911b5d3d6d867ef5f452ad1fa492a0a
Craig A. McDonald <craig@mcdonald.codes>
2020-10-01 20:09:53 +0100

    Java difference of squares problem completed

---
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -0,0 +1,30 @@
+import java.util.stream.IntStream;
+
+class DifferenceOfSquaresCalculator {
+
+    ////////////////////////////////////////////////////////////////////////////////
+    int computeSquareOfSumTo(int input) {
+
+        int sum = IntStream.rangeClosed(1, input)
+                           .sum();
+
+        return (int)Math.pow(sum ,2);
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    int computeSumOfSquaresTo(int input) {
+
+        return IntStream.rangeClosed(1, input)
+                        .map(s -> (int)Math.pow(s, 2))
+                        .sum();
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    int computeDifferenceOfSquares(int input) {
+
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+    }
+
+}
