677d64e7443878fc72a7a7a0c48f2ad997d8b20f
Craig A. McDonald <craig@mcdonald.codes>
2020-10-04 19:25:47 +0100

    Java luhn validator two tests left

--- java/luhn/src/main/java/LuhnValidator.java
+++ java/luhn/src/main/java/LuhnValidator.java
@@ -4,15 +4,48 @@ import java.util.Objects;
 class LuhnValidator {
 
     private final int MIN_LENGTH = 2;
+    private final int MAX_VALUE = 9;
 
-    boolean isValid(String candidate) {
+    boolean isValid(final String candidate) {
 
         // Early return
         if (candidate == null || candidate.length() < MIN_LENGTH) {
             return false;
         }
 
-        // Not really but lets just pass the first tests
-        return true;
+        int number = 0;
+
+        try {
+            number = Integer.parseInt(candidate.replace(" ", ""));
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        int sum = 0;
+        boolean mustDouble = false;
+
+        while (number > 0) {
+
+            int end = number % 10;
+
+            if (mustDouble) {
+
+                end *= 2;
+
+                if (end > MAX_VALUE) {
+                    end -= MAX_VALUE;
+                }
+
+                mustDouble = false;
+            } else {
+                mustDouble = true;
+            }
+
+            sum += end;
+
+            number /= 10;
+        }
+
+        return sum % 10 == 0;
     }
 }
