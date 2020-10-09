53010ef04f2f622be65e00a1608ae807762fb25b
Craig A. McDonald <craig@mcdonald.codes>
2020-10-09 23:24:44 +0100

    Java diamond printer started

---
+++ java/diamond/src/main/java/DiamondPrinter.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+class DiamondPrinter {
+
+    private final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    List<String> printToList(char a) {
+
+        int index = alphabet.indexOf(a);
+
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+}
