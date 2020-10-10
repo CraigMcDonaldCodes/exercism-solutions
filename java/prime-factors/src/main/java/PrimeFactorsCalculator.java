1bedaa65d34405ca30c663caccf4b8502bf2a598
Craig A. McDonald <craig@mcdonald.codes>
2020-10-10 20:04:05 +0100

    Java prime factors calculator started

---
+++ java/prime-factors/src/main/java/PrimeFactorsCalculator.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactorsCalculator {
+
+    public List<Long> calculatePrimeFactorsOf(final long value) {
+
+        final var list = new ArrayList<Long>();
+        long upperLimit = value / 2;
+
+        for (int i = 2; i <= upperLimit; i++) {
+        }
+
+        return list;
+    }
+}
