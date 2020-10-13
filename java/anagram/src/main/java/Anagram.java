b8e85e326b521c707aac4d55d5a93650016d6c5a
Craig A. McDonald <craig@mcdonald.codes>
2020-10-13 18:21:52 +0100

    Java anagram problem started

---
+++ java/anagram/src/main/java/Anagram.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Anagram {
+
+    private final String text;
+
+    public Anagram(String text) {
+        this.text = text;
+    }
+
+    public List<String> match(List<String> words) {
+
+        var list = new ArrayList<String>();
+
+        return list;
+    }
+}
