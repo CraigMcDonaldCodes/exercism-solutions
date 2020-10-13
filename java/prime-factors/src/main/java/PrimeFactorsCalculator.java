0644c8536fcaea1d685e2e93b64c970450d08938
Craig A. McDonald <craig@mcdonald.codes>
2020-10-13 18:09:52 +0100

    Java prime factors calculator completed

--- java/prime-factors/src/main/java/PrimeFactorsCalculator.java
+++ java/prime-factors/src/main/java/PrimeFactorsCalculator.java
@@ -1,10 +1,6 @@
-// This isn't actually a correct answer, it just
-// passes all the Exercism tests
-// but I don't want to cheat and look at some of the
-// submitted solutions.
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
 
 public class PrimeFactorsCalculator {
 
@@ -18,20 +14,70 @@ public class PrimeFactorsCalculator {
             return list;
         }
 
-        // Check the only even prime
-        while (currentValue % 2 == 0) {
-            list.add(2L);
-            currentValue /= 2;
-        }
+        var primes = new PrimesIterator();
 
-        for (long i = 3; i <= upperLimit && currentValue != 0; i += 2) {
+        while (currentValue > 1 && primes.hasNext()) {
 
-            while (currentValue % i == 0) {
-                list.add(i);
-                currentValue /= i;
+            long prime = primes.next();
+
+            while (currentValue % prime == 0) {
+                list.add(prime);
+                currentValue /= prime;
             }
         }
 
         return list;
+    }
+}
+
+class PrimesIterator implements Iterator<Long> {
+
+    private static final Long PRIME_INCREMENT = 2L;
+    private final List<Long> primes;
+
+    public PrimesIterator() {
+
+        primes = new ArrayList<>();
+    }
+
+    @Override
+    public boolean hasNext() {
+
+        if (primes.size() == 0) {
+            return true;
+        } else {
+            return primes.get(primes.size() - 1) < Long.MAX_VALUE;
+        }
+    }
+
+    @Override
+    public Long next() {
+
+        // The first two primes
+        if (primes.size() == 0) { primes.add(2L); return 2L; }
+        if (primes.size() == 1) { primes.add(3L); return 3L; }
+
+        long possiblePrime = primes.get(primes.size() - 1);
+        boolean notFound = true;
+
+        while (notFound) {
+
+            possiblePrime += PRIME_INCREMENT;
+
+            for (long p : primes) {
+
+                if (possiblePrime % p == 0) {
+                    break;
+                } else {
+                    notFound = false;
+                    break;
+                }
+            }
+        }
+
+        long nextPrime = possiblePrime;
+        primes.add(nextPrime);
+
+        return nextPrime;
     }
 }
