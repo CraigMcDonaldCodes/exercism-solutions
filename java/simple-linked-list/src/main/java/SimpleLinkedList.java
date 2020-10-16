1e7b259bc2d8af86b1a82a4bf022ea8a4020773f
Craig A. McDonald <craig@mcdonald.codes>
2020-10-16 23:58:54 +0100

    Java simple linked list started

---
+++ java/simple-linked-list/src/main/java/SimpleLinkedList.java
@@ -0,0 +1,18 @@
+public class SimpleLinkedList<T> {
+
+    public SimpleLinkedList() {
+    }
+
+    public SimpleLinkedList(T[] data) {
+    }
+
+    public T pop() {
+
+        return null;
+    }
+
+    public int size() {
+
+        return -1;
+    }
+}
