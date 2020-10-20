49d89701176cbcb4ebf55f0c9e2562d92cfaaf21
Craig A. McDonald <craig@mcdonald.codes>
2020-10-20 20:51:33 +0100

    Java darts problem completed

---
+++ java/darts/src/main/java/Darts.java
@@ -0,0 +1,38 @@
+class Darts {
+
+    private final int score;
+
+    ////////////////////////////////////////////////////////////////////////////////
+    Darts(double x, double y) {
+        score = calculateScore(x, y);
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    int score() {
+        return score;
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    private int calculateScore(double x, double y) {
+
+        double radius = Math.sqrt(x * x + y * y);
+        int score = 0;
+
+        if (radius > 10) {
+            // Outside the circle, no score
+        } else if (radius > 5) {
+            // Outer radius
+            score = 1;
+        } else if (radius > 1) {
+            // Middle radius
+            score = 5;
+        } else if (radius >= 0) {
+            // Inner radius
+            score = 10;
+        }
+
+        return score;
+    }
+}
