f05ec5ce4acd02720d1aa6881f0c248112e1d579
Craig A. McDonald <craig@mcdonald.codes>
2020-11-07 21:16:04 +0000

    C space age problem completed

--- c/space-age/src/space_age.c
+++ c/space-age/src/space_age.c
@@ -1,26 +1,33 @@
-#include <stdint.h>
 #include "space_age.h"
 
 float convert_planet_age(planet_t planet, int64_t input) {
 
-    float result = 0.0F;
+    float result = input / EARTH_SEC;
 
     switch (planet) {
+        case EARTH:
+            // Do nothing
+            break;
         case MERCURY:
+            result /= MERCURY_EARTH_YEAR;
             break;
         case VENUS:
-            break;
-        case EARTH:
+            result /= VENUS_EARTH_YEAR;
             break;
         case MARS:
+            result /= MARS_EARTH_YEAR;
             break;
         case JUPITER:
+            result /= JUPITER_EARTH_YEAR;
             break;
         case SATURN:
+            result /= SATURN_EARTH_YEAR;
             break;
         case URANUS:
+            result /= URANUS_EARTH_YEAR;
             break;
         case NEPTUNE:
+            result /= NEPTUNE_EARTH_YEAR;
             break;
     }
 
--- c/space-age/src/space_age.h
+++ c/space-age/src/space_age.h
@@ -3,19 +3,25 @@
 
 #include <stdint.h>
 
-#define MERCURY 0.2408467
-#define VENUS 0.61519726
-#define EARTH 
+#define EARTH_SEC 31557600
+
+#define MERCURY_EARTH_YEAR  0.2408467
+#define VENUS_EARTH_YEAR    0.61519726
+#define MARS_EARTH_YEAR     1.8808158
+#define JUPITER_EARTH_YEAR  11.862615
+#define SATURN_EARTH_YEAR   29.447498
+#define URANUS_EARTH_YEAR   84.016846
+#define NEPTUNE_EARTH_YEAR  164.79132
 
 typedef enum planet {
-   MERCURY,
-   VENUS,
-   EARTH,
-   MARS,
-   JUPITER,
-   SATURN,
-   URANUS,
-   NEPTUNE,
+    MERCURY,
+    VENUS,
+    EARTH,
+    MARS,
+    JUPITER,
+    SATURN,
+    URANUS,
+    NEPTUNE,
 } planet_t;
 
 float convert_planet_age(planet_t planet, int64_t input);
