5031c01bee76b957a28f82d6550a7690fa24c0dc
Craig A. McDonald <craig@mcdonald.codes>
2020-11-07 22:57:10 +0000

    C resistor color problem updated

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -1,1 +1,14 @@
 #include "resistor_color.h"
+
+int color_code(resistor_band_t resistor) {
+    return (int)resistor;
+}
+
+resistor_band_t* colors(void) {
+
+    resistor_band_t data[] = {
+        BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE
+    };
+
+    return data;
+}
