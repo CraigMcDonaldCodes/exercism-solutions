74121ff7b1d0c8d580c763b6a92470f0663f4c73
Craig A. McDonald <craig@mcdonald.codes>
2020-11-14 19:29:41 +0000

    Python scrabble score completed

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,23 @@
 def score(word):
-    pass
+
+    score = 0
+
+    for w in word.upper():
+
+        if w in ('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'):
+            score += 1
+        elif w in ('D', 'G'):
+            score += 2
+        elif w in ('B', 'C', 'M', 'P'):
+            score += 3
+        elif w in ('F', 'H', 'V', 'W', 'Y'):
+            score += 4
+        elif w == 'K':
+            score += 5
+        elif w in ('J', 'X'):
+            score += 8
+        elif w in ('Q', 'Z'):
+            score += 10
+
+    return score
+
