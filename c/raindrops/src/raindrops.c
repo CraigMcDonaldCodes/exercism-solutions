36d89b43643849c383517f3f3a29423905423e37
Craig A. McDonald <craig@mcdonald.codes>
2020-12-28 14:39:34 +0000

    C raindrops problem completed

--- c/raindrops/src/raindrops.c
+++ c/raindrops/src/raindrops.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 #include "raindrops.h"
+
+char* convert(char result[], int drops) {
+
+    /* Should use 'strcat_s' so we will have to assume char array is large enough */
+
+    _Bool matched = false;
+
+    if (drops % 3 == 0) {
+        strcat(result, "Pling");
+        matched = true;
+    }
+
+    if (drops % 5 == 0) {
+        strcat(result, "Plang");
+        matched = true;
+    }
+
+    if (drops % 7 == 0) {
+        strcat(result, "Plong");
+        matched = true;
+    }
+
+    /* No match so return int back as char array */
+    if (!matched) {
+        sprintf(result, "%d", drops);
+    }
+
+    return result;
+}
--- c/raindrops/test/test_raindrops.c
+++ c/raindrops/test/test_raindrops.c
@@ -26,91 +26,76 @@ static void test_one_yields_itself(void)
 
 static void test_three_yields_pling(void)
 {
-   TEST_IGNORE();               // delete this line to run test
    convert_drops(3, "Pling");
 }
 
 static void test_five_yields_plang(void)
 {
-   TEST_IGNORE();
    convert_drops(5, "Plang");
 }
 
 static void test_seven_yields_plong(void)
 {
-   TEST_IGNORE();
    convert_drops(7, "Plong");
 }
 
 static void test_six_yields_pling(void)
 {
-   TEST_IGNORE();
    convert_drops(6, "Pling");
 }
 
 static void test_nine_yields_pling(void)
 {
-   TEST_IGNORE();
    convert_drops(9, "Pling");
 }
 
 static void test_ten_yields_plang(void)
 {
-   TEST_IGNORE();
    convert_drops(10, "Plang");
 }
 
 static void test_fourteen_yields_plong(void)
 {
-   TEST_IGNORE();
    convert_drops(14, "Plong");
 }
 
 static void test_fifteen_yields_plingplang(void)
 {
-   TEST_IGNORE();
    convert_drops(15, "PlingPlang");
 }
 
 static void test_twenty_one_yields_plingplong(void)
 {
-   TEST_IGNORE();
    convert_drops(21, "PlingPlong");
 }
 
 static void test_twenty_five_yields_plang(void)
 {
-   TEST_IGNORE();
    convert_drops(25, "Plang");
 }
 
 static void test_thirty_five_yields_plangplong(void)
 {
-   TEST_IGNORE();
    convert_drops(35, "PlangPlong");
 }
 
 static void test_forty_nine_yields_plong(void)
 {
-   TEST_IGNORE();
    convert_drops(49, "Plong");
 }
 
 static void test_fifty_two_yields_itself(void)
 {
-   TEST_IGNORE();
    convert_drops(52, "52");
 }
 
 static void test_one_hundred_five_yields_plingplangplong(void)
 {
-   TEST_IGNORE();
    convert_drops(105, "PlingPlangPlong");
 }
 
 static void test_big_prime_yields_itself(void)
 {
-   TEST_IGNORE();
    convert_drops(12121, "12121");
 }
 
