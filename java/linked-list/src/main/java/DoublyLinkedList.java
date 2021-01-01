8c23a1a6e46873a1f7ed13e61619880b5ddcdf1f
Craig A. McDonald <craig@mcdonald.codes>
2021-01-01 20:28:44 +0000

    Java linked list updated attempt

--- java/linked-list/src/main/java/DoublyLinkedList.java
+++ java/linked-list/src/main/java/DoublyLinkedList.java
@@ -1,19 +1,51 @@
 public class DoublyLinkedList<T> {
 
+    private static final int INITIAL_SIZE = 16;
+    private Node<T> head;
+    private Node<T> current;
+
     public DoublyLinkedList() {
+
+        // head = null;
+        // current = null;
     }
 
     public void push(T value) {
+
+        if (head == null) {
+            head = new Node<T>(value);
+            current = head;
+        } else {
+            var newNode = new Node<T>(value);
+            current.setNext(newNode);
+            current = newNode;
+        }
     }
 
     public T pop() {
-        return null;
+
+        // TODO Check for no head Node
+
+        T value = current.getValue();
+        Node<T> tmp = current.getPrevious();
+        current = tmp;
+
+        return value;
     }
 
     public T shift() {
-        return null;
+
+        T value = head.getValue();
+        Node<T> newHead = head.getNext();
+        head = newHead;
+
+        return value;
     }
 
-    public void unshift() {
+    public void unshift(T value) {
+
+        var newHead = new Node<T>(value);
+        newHead.setNext(head);
+        head = newHead;
     }
 }
