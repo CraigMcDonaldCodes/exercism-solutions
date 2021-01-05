215ab6d6272177961fa3085a6a6795d3ede6d7d1
Craig A. McDonald <craig@mcdonald.codes>
2021-01-05 18:42:44 +0000

    Go grains problem completed

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,1 +1,24 @@
 package grains
+
+import (
+	"errors"
+	"math"
+)
+
+const MIN_SQUARES int = 1
+const MAX_SQUARES int = 64
+
+func Square(n int) (uint64, error) {
+
+	if (n < MIN_SQUARES) || (n > MAX_SQUARES) {
+		return 0, errors.New("Out of range")
+	}
+
+	return uint64(math.Pow(2.0, float64(n-1))), nil
+}
+
+func Total() uint64 {
+
+	return (1 << 64) - 1
+}
+
