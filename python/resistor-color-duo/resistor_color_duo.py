249524d6010434433a5d289c18695bb50f31d464
Craig A. McDonald <craig@mcdonald.codes>
2021-01-29 23:51:13 +0000

    Python resistor color duo completed

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,32 @@
+all_colors = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white"
+]
+
 def value(colors):
-    pass
+
+    if len(colors) < 2:
+        raise ValueError("Must supply two values.")
+
+    total = 0
+
+    for i in range(2):
+
+        total *= 10
+
+        if colors[i] in all_colors:
+            total += all_colors.index(colors[i])
+        else:
+            raise ValueError("{} is not a valid color".format(colors[i]))
+
+
+    return total
+
