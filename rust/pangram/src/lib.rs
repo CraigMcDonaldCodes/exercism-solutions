e384d796f2638f0248dddf188e7e18d643d14247
Craig A. McDonald <craig@mcdonald.codes>
2021-01-31 17:30:57 +0000

    Rust pangram problem updated
    Passing all tests apart from single unicode, might have to solve this another way.

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,4 +1,19 @@
 /// Determine whether a sentence is a pangram.
+use std::collections::HashSet;
+
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {} a pangram?", sentence);
+
+    // unimplemented!("Is {} a pangram?", sentence);
+
+    let mut set = HashSet::new();
+
+    for c in sentence.chars() {
+
+        if c.is_ascii_alphabetic() {
+            set.insert(c);
+        }
+    }
+
+    // println!("{}", set.len());
+    set.len() == 26
 }
