31b92ebe71c516d7f51dffd84ded94d17acce650
Craig A. McDonald <craig@mcdonald.codes>
2021-02-02 23:20:21 +0000

    Python grains problem solved

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,15 @@
+__MIN = 1
+__MAX = 64
+__TOTAL = (2 << 63) - 1
+
+
 def square(number):
-    pass
+
+    if number < __MIN or number > __MAX:
+        raise ValueError("Value out of range")
+
+    return 2 ** (number - 1)
 
 
 def total():
-    pass
+    return __TOTAL
