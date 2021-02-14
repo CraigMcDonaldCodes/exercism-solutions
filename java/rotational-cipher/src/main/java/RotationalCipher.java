c4ca43c26a7ee8ead370cea3276421aa7a71f068
Craig A. McDonald <craig@mcdonald.codes>
2021-02-14 21:57:05 +0000

    Java rotational cipher problem naive attempt and failing some tests

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -1,11 +1,31 @@
-class RotationalCipher {
+public class RotationalCipher {
+
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 26;
+    private final int key;
 
     RotationalCipher(int shiftKey) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        if (shiftKey < MIN_VALUE || shiftKey > MAX_VALUE) { throw new IllegalArgumentException(); }
+        key = shiftKey;
     }
 
-    String rotate(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
+    public String rotate(String data) {
 
+        // No rotation
+        if (key == MIN_VALUE || key == MAX_VALUE) { return data; }
+
+        var sb = new StringBuilder();
+
+        for (var c : data.toCharArray()) {
+
+            if (Character.isAlphabetic(c)) {
+                sb.append((char)(c + key));
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
 }
