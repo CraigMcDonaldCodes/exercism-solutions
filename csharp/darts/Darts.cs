5fe854e07f3dfdaf89f5fefb9ae4709ff12ffaf7
Craig A. McDonald <craig@mcdonald.codes>
2021-03-13 20:39:09 +0000

    C# darts problem completed

--- csharp/darts/Darts.cs
+++ csharp/darts/Darts.cs
@@ -2,8 +2,35 @@
 
 public static class Darts
 {
+    private const int OuterScore = 1;
+    private const int MiddleScore = 5;
+    private const int InnerScore = 10;
+
     public static int Score(double x, double y)
     {
-       throw new NotImplementedException("You need to implement this function");
+        var radius = Math.Sqrt(x * x + y *y);
+        int score = 0;
+
+        if (radius > 10)
+        {
+            // Outside the circle, no score
+        }
+        else if (radius > 5)
+        {
+            // Outer radius
+            score = OuterScore;
+        }
+        else if (radius > 1)
+        {
+            // Middle radius
+            score = MiddleScore;
+        }
+        else if (radius >= 0)
+        {
+            // Inner radius
+            score = InnerScore;
+        }
+       
+        return score;
     }
 }
