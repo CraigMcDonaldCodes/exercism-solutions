7c40c71b3bf29e65e4d8954899f5c8488c62da24
Craig A. McDonald <craig@mcdonald.codes>
2021-03-13 20:10:03 +0000

    C# resistor color duo problem completed

--- csharp/resistor-color-duo/ResistorColorDuo.cs
+++ csharp/resistor-color-duo/ResistorColorDuo.cs
@@ -2,8 +2,13 @@ using System;
 
 public static class ResistorColorDuo
 {
+    private static readonly string[] _colors =
+        { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+
     public static int Value(string[] colors)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Array.IndexOf(_colors, colors[0]) * 10 +
+               Array.IndexOf(_colors, colors[1]);
+
     }
 }
