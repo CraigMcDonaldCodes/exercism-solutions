956d43ba78fdd4d2004ffdc7c26b05479fc0cd91
Craig McDonald <craig@mcdonald.codes>
2021-04-04 15:49:27 +0100

    Go darts problem completed

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,1 +1,31 @@
 package darts
+
+import "math"
+
+const (
+	noScore     = 0
+	outerScore  = 1
+	middleScore = 5
+	innerScore  = 10
+)
+
+func Score(x, y float64) int {
+
+	radius := math.Sqrt(x*x + y*y)
+	var score int
+
+	switch {
+	case radius > 10:
+		score = noScore
+	case radius > 5:
+		score = outerScore
+	case radius > 1:
+		score = middleScore
+	case radius >= 0:
+		score = innerScore
+	default:
+		score = noScore
+	}
+
+	return score
+}
