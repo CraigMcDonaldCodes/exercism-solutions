ab2de4d477602fc2ec0de4ca0b83922c1a50b8b5
Craig McDonald <craig@mcdonald.codes>
2021-06-06 19:48:16 +0100

    JavaScript darts problem solved

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -1,8 +1,20 @@
-//
-// This is only a SKELETON file for the 'Darts' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const score = (x, y) => {
 
-export const score = () => {
-  throw new Error('Remove this statement and implement this function');
+  let radius = Math.sqrt(x * x + y * y);
+  let score = 0;
+
+  if (radius > 10) {
+    // Outside the circle, no score
+  } else if (radius > 5) {
+    // Outer radius
+    score = 1;
+  } else if (radius > 1) {
+    // Middle radius
+    score = 5;
+  } else if (radius >= 0) {
+    // Inner radius
+    score = 10;
+  }
+
+  return score;
 };
