46e104e81e9b1e0661bec8ba39900568525f4418
Craig McDonald <craig@mcdonald.codes>
2021-06-11 17:54:45 +0100

    Rust sum of multiples problem solved

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,7 +1,21 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+
+    // There is definitely a more succint way of doing this
+    // probably a poor way of solving
+    // I will come back to it once I know Rust a bit better
+
+    let mut sum = 0u32;
+
+    for n in 1..limit {
+
+        for f in factors {
+
+            if (*f != 0u32) && n % f == 0 {
+                sum += n;
+                break
+            }
+        }
+    }
+
+    sum
 }
