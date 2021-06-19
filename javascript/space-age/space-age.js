cba85832252f8c5bd4d7c683c4a70f91bb1b1c03
Craig McDonald <craig@mcdonald.codes>
2021-06-19 19:45:23 +0100

    JavaScript space age problem completed

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,8 +1,21 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const planets = new Map([
+    [ "earth",   31_557_600],
+    [ "mercury", 0.2_408_467],
+    [ "venus",   0.61_519_726],
+    [ "mars",    1.8_808_158],
+    [ "jupiter", 11.8_626_15],
+    [ "saturn",  29.447_498],
+    [ "uranus",  84.016_846],
+    [ "neptune", 164.79_132],
+]);
 
-export const age = () => {
-  throw new Error("Remove this statement and implement this function");
+export const age = (planet, seconds) => {
+
+  let answer = seconds / planets.get("earth");
+
+  if (planet !== "earth" && planets.has(planet)) {
+    answer /= planets.get(planet);
+  }
+
+  return +(answer.toFixed(2));
 };
