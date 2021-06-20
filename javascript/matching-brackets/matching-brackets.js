bbc80296d7edd2f047c6b1a84e9cfb2c83002c71
Craig McDonald <craig@mcdonald.codes>
2021-06-20 22:22:12 +0100

    JavaScript matching brackets problem solved

--- javascript/matching-brackets/matching-brackets.js
+++ javascript/matching-brackets/matching-brackets.js
@@ -1,8 +1,37 @@
-//
-// This is only a SKELETON file for the 'Matching Brackets' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const closingChar = new Map([
+  ["]", "["],
+  ["}", "{"],
+  [")", "("],
+]);
 
-export const isPaired = () => {
-  throw new Error('Remove this statement and implement this function');
+// Will have already checked length > 0 before calling
+const peek = (stack) => stack[stack.length - 1];
+
+const isStackEmpty = stack => stack.length == 0;
+
+export const isPaired = (text) => {
+
+  let stack = [];
+
+  for (const c of text) {
+
+    switch (c) {
+      case "[":
+      case "{":
+      case "(":
+        stack.push(c);
+        break;
+      case "]":
+      case "}":
+      case ")":
+        if (stack.length > 0 && peek(stack) == closingChar.get(c)) {
+          stack.pop();
+        } else {
+          return false;
+        }
+        break;
+    }
+  }
+
+  return isStackEmpty(stack);
 };
