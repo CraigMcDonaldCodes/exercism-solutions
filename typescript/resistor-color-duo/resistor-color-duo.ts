ed50d0e756ff738b2aa9d1f0c77597f5405a2c51
Craig McDonald <craig@mcdonald.codes>
2021-06-27 13:18:48 +0100

    TypeScript resistor color duo problem completed

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,26 @@
 export class ResistorColor {
+
+  static readonly AllColors: string[] = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ]
+
   private colors: string[]
 
   constructor(colors: string[]) {
+    if (colors.length < 2) { throw new Error('At least two colors need to be present') }
     this.colors = colors
   }
-  value = (): number => 0
+
+  value = (): number => {
+    return ResistorColor.AllColors.indexOf(this.colors[0]) * 10 + ResistorColor.AllColors.indexOf(this.colors[1])
+  }
 }
