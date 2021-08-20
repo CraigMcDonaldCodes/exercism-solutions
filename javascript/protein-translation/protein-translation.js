6fe16b5ecfd26e728007655f6d846d74e12bb8ae
Craig McDonald <craig@mcdonald.codes>
2021-08-20 20:23:51 +0100

    JavaScript protein translation problem update

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -1,8 +1,36 @@
-//
-// This is only a SKELETON file for the 'Protein Translation' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const map = new Map();
+map.set("AUG", "Methionine");
+map.set("UUU", "Phenylalanine");
+map.set("UUC", "Phenylalanine");
+map.set("UUA", "Leucine");
+map.set("UUG", "Leucine");
+map.set("UCU", "Serine");
+map.set("UCC", "Serine");
+map.set("UCA", "Serine");
+map.set("UCG", "Serine");
+map.set("UAU", "Tyrosine");
+map.set("UAC", "Tyrosine");
+map.set("UGU", "Cysteine");
+map.set("UGC", "Cysteine");
+map.set("UGG", "Tryptophan");
+map.set("UAA", "STOP");
+map.set("UAG", "STOP");
+map.set("UGA", "STOP");
 
-export const translate = () => {
-  throw new Error('Remove this statement and implement this function');
+
+export const translate = (rna) => {
+
+    let rnaParts = rna.math(/[A-Z]{3}/g);
+    let proteins = [];
+
+    for (const part of rnaParts) {
+
+      if (map.has(part)) {
+        proteins.push(part);
+      } else {
+        throw new Error('Invalid codon');
+      }
+    }
+
+    return proteins;
 };
