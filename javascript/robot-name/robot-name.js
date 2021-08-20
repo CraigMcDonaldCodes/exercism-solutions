42e7058b29cb25cfcae32b2ae7a390a885b25d74
Craig McDonald <craig@mcdonald.codes>
2021-08-20 18:20:58 +0100

    JavaScript robot name first attempt at problem

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,6 +1,56 @@
-// This is only a SKELETON file for the 'Robot Name' exercise. It's been
-// provided as a convenience to get your started writing code faster.
+'use strict';
 
-export class Robot {}
+export class Robot {
 
-Robot.releaseNames = () => {};
+    constructor() {
+        this.robotNames = new Set();
+        this.name = this.getNextName();
+    }
+
+    random(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    generateName() {
+
+        let newName = "";
+
+        // Two uppercase chars
+        newName += String.fromCharCode(this.random(1, 26) + 65);
+        newName += String.fromCharCode(this.random(1, 26) + 65);
+
+        // Three numbers
+        newName += this.random(0, 10);
+        newName += this.random(0, 10);
+        newName += this.random(0, 10);
+
+        return newName;
+    }
+
+    getNextName() {
+
+        let nextName = this.generateName();
+
+        while (this.robotNames.has(nextName)) {
+            nextName = this.generateName();
+        }
+
+        this.robotNames.add(nextName);
+
+        return nextName;
+    }
+
+    releaseNames() {
+        this.robotNames.clear();
+    }
+
+    reset() {
+        this.name = this.getNextName();
+    }
+}
+
+/*
+Robot.releaseNames = () => {
+        robotNames.clear();
+};
+*/
