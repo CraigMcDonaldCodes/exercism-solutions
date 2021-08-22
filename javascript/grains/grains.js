b2a0cf494ebbd40a1c32e1420e88ab1b16b8fada
Craig McDonald <craig@mcdonald.codes>
2021-08-22 18:10:04 +0100

    JavaScript grains problem 9/11 tests passing

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -1,9 +1,16 @@
 'use strict';
 
-export const square = () => {
-  throw new Error('Remove this statement and implement this function');
+export const square = (n) => {
+  // throw new Error('Remove this statement and implement this function');
+
+  if (n < 1 || n > 64) {
+    throw new Error('square must be between 1 and 64');
+  }
+
+  return Math.pow(2, n - 1);
 };
 
-export const total = () => {
-  throw new Error('Remove this statement and implement this function');
+export const total = (n) => {
+  // throw new Error('Remove this statement and implement this function');
+  return Math.pow(2, 64) - 1;
 };
