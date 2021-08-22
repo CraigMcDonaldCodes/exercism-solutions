f185fdd5c51f13c796078cc9d13f194e9b25ab5b
Craig McDonald <craig@mcdonald.codes>
2021-08-22 18:45:41 +0100

    JavaScript grains problem solved

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -1,16 +1,24 @@
 'use strict';
 
-export const square = (n) => {
-  // throw new Error('Remove this statement and implement this function');
+// We should probably just hardcode the upper values
+// that require BigInt
 
-  if (n < 1 || n > 64) {
+const MIN_VALUE = 1;
+const MAX_VALUE = 64;
+
+export const square = (n) => {
+
+  if (n < MIN_VALUE || n > MAX_VALUE) {
     throw new Error('square must be between 1 and 64');
   }
 
-  return Math.pow(2, n - 1);
+  if (n == MAX_VALUE) {
+    return BigInt(2) ** BigInt(MAX_VALUE - 1);
+  } else {
+    return Math.pow(2, n - 1);
+  }
 };
 
 export const total = (n) => {
-  // throw new Error('Remove this statement and implement this function');
-  return Math.pow(2, 64) - 1;
+  return BigInt(2) ** BigInt(MAX_VALUE) - BigInt(1);
 };
