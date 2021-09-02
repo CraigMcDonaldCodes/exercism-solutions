9ccf552081a0246abb0ec88edd6bdd5ba1eeca0b
Craig McDonald <craig@mcdonald.codes>
2021-09-02 19:28:20 +0100

    Java lasagna problem started

---
+++ java/lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,10 @@
+public class Lasagna {
+
+    // TODO: define the 'expectedMinutesInOven()' method
+
+    // TODO: define the 'remainingMinutesInOven()' method
+
+    // TODO: define the 'preparationTimeInMinutes()' method
+
+    // TODO: define the 'totalTimeInMinutes()' method
+}
