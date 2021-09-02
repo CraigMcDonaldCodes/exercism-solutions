b584791ce5f4ad22167686d3fc3b2e834b77c902
Craig McDonald <craig@mcdonald.codes>
2021-09-02 22:12:06 +0100

    Java lasagna problem solved

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,10 +1,25 @@
 public class Lasagna {
 
-    // TODO: define the 'expectedMinutesInOven()' method
+    // Should probably make sure method
+    // inputs are within reasonable range
+    // but supplied tests are passing
 
-    // TODO: define the 'remainingMinutesInOven()' method
+    private static final int COOKING_TIME = 40;
+    private static final int MINUTES_PER_LAYER = 2;
 
-    // TODO: define the 'preparationTimeInMinutes()' method
+    public int expectedMinutesInOven() {
+        return COOKING_TIME;
+    }
 
-    // TODO: define the 'totalTimeInMinutes()' method
+    public int remainingMinutesInOven(int time) {
+        return COOKING_TIME - time;
+    }
+
+    public int preparationTimeInMinutes(int layers) {
+        return MINUTES_PER_LAYER * layers;
+    }
+
+    public int totalTimeInMinutes(int layers, int time) {
+        return time + preparationTimeInMinutes(layers);
+    }
 }
