c6e0be49b60c15ce78248ab58a2f4eb813fe08b2
Craig McDonald <craig@mcdonald.codes>
2021-09-04 16:20:31 +0100

    Java cars assemble problem completed

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,42 @@
 public class CarsAssemble {
 
+    private static final int MIN_SPEED = 0;
+    private static final int MAX_SPEED = 10;
+    private static final int CARS_PER_HOUR = 221;
+
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.productionRateperHour() method");
+
+        validateSpeed(speed);
+
+        double successRate = 1.0;
+
+        switch (speed) {
+
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+                successRate = 0.90;
+                break;
+            case 9:
+                successRate = 0.80;
+                break;
+            case 10:
+                successRate = 0.77;
+                break;
+        }
+
+        return speed * CARS_PER_HOUR * successRate;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the AssemblyLine.workingItemsPerMinute() method");
+        return (int)(productionRatePerHour(speed) / 60);
+    }
+
+    private void validateSpeed(int speed) {
+
+        if (speed < MIN_SPEED || speed > MAX_SPEED) {
+            throw new IllegalArgumentException("Invalid speed: range is 0 - 10");
+        }
     }
 }
