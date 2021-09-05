01a327f0b59f2177fb6fe6900c8091ce95e7353d
Craig McDonald <craig@mcdonald.codes>
2021-09-05 22:12:02 +0100

    Java log levels problem completed

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,45 @@
+import java.util.Objects;
+
 public class LogLevels {
-    
+
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+
+        Objects.requireNonNull(logLine);
+
+        var message = logLine.substring(logLine.indexOf(" ") + 1);
+        message = message.trim();
+
+        return message;
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+
+        Objects.requireNonNull(logLine);
+        String logLevel = null;
+
+        if (logLine.contains("INFO")) {
+            logLevel = "info";
+        }
+
+        if (logLine.contains("ERROR")) {
+            logLevel = "error";
+        }
+
+        if (logLine.contains("WARNING")) {
+            logLevel = "warning";
+        }
+
+        return logLevel;
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+
+        Objects.requireNonNull(logLine);
+
+        return String.format(
+            "%s (%s)",
+            message(logLine),
+            logLevel(logLine)
+        );
     }
 }
