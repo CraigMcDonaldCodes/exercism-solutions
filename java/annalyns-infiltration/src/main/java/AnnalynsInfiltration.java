c2701146e2bf2c6d56d39696faf2239148e769c9
Craig McDonald <craig@mcdonald.codes>
2021-09-06 21:50:33 +0100

    Java annalyns infiltration problem completed

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -1,17 +1,27 @@
 class AnnalynsInfiltration {
+
     public static boolean canFastAttack(boolean knightIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) QuestLogic.canFastAttack() method");
+        return !knightIsAwake;
     }
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) QuestLogic.canSpy() method");
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException("Please implement the (static) QuestLogic.canSignalPrisoner() method");
+        return !archerIsAwake && prisonerIsAwake;
     }
 
     public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        throw new UnsupportedOperationException("Please implement the (static) QuestLogic.canFreePrisoner() method");
+
+        if (prisonerIsAwake && !knightIsAwake && !archerIsAwake) {
+            return true;
+        }
+
+        if (!archerIsAwake && petDogIsPresent) {
+            return true;
+        }
+
+        return false;
     }
 }
