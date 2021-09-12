bde8defa2bdb73f456ff9a6280ced7a9c3b417ec
Craig McDonald <craig@mcdonald.codes>
2021-09-12 16:03:15 +0100

    Go lasagna problem clean up

--- go/lasagna/lasagna.go
+++ go/lasagna/lasagna.go
@@ -3,22 +3,18 @@ package lasagna
 const cookingTime = 40
 const minutesPerLayer = 2
 
-// TODO: define the 'OvenTime()' function
 func OvenTime() int {
 	return cookingTime
 }
 
-// TODO: define the 'RemainingOvenTime()' function
 func RemainingOvenTime(time int) int {
 	return cookingTime - time
 }
 
-// TODO: define the 'PreparationTime()' function
 func PreparationTime(layers int) int {
 	return minutesPerLayer * layers
 }
 
-// TODO: define the 'ElapsedTime()' function
 func ElapsedTime(layers, time int) int {
 	return time + PreparationTime(layers)
 }
