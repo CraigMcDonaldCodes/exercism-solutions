0446008c22889360ecf40367684cbf0a6a9c9f35
Craig McDonald <craig@mcdonald.codes>
2021-09-14 15:41:07 +0100

    Go weather forecast solved

--- go/weather-forecast/weather.go
+++ go/weather-forecast/weather.go
@@ -1,10 +1,15 @@
+// Package weather does misc. weather stuff.
+// Who knows really, it's just a small problem to solve.
 package weather
 
 var (
+	// Do something with the current condition
 	CurrentCondition string
-	CurrentLocation  string
+	// Do something with the current location
+	CurrentLocation string
 )
 
+// Log - A function so simple it would never be required in a real project
 func Log(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
