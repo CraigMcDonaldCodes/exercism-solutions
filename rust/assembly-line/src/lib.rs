86006efe39136134591013e7e8079692bba624ad
Craig McDonald <craig@mcdonald.codes>
2021-09-17 13:05:23 +0100

    Rust assembly line solved

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,11 +1,18 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
+const MIN_SPEED: u32 = 0;
+const MAX_SPEED: u32 = 10;
+const CARS_PER_HOUR: u32 = 221;
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+
+    let success_rate = match speed {
+        5..=8 => 0.9,
+        9 | 10 => 0.77,
+        _ => 1.0
+    };
+
+    (speed as u32 * CARS_PER_HOUR) as f64 * success_rate
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) / 60.0) as u32
 }
