02f07b84e99e87801b6b7727305263d77ecbb0b7
Craig McDonald <craig@mcdonald.codes>
2021-11-02 11:52:19 +0000

    JavaScript poetry club door policy completed

--- javascript/poetry-club-door-policy/door-policy.js
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -1,23 +1,4 @@
 // @ts-check
-//
-// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
-// the @ts-check directive. It will give you helpful autocompletion on the web
-// and supported IDEs when implementing this exercise. You don't need to
-// understand types, JSDoc, or TypeScript in order to complete this JavaScript
-// exercise, and can completely ignore this comment block and directive.
-//
-// 👋🏽 Hi again!
-//
-// A quick reminder about exercise stubs:
-//
-// 💡 You're allowed to completely clear any stub before you get started. Often
-// we recommend using the stub, because they are already set-up correctly to
-// work with the tests, which you can find in ./door-policy.spec.js.
-//
-// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
-// in idiomatic JavaScript, but some companies and style-guides do enforce them.
-//
-// Good luck with that door policy!
 
 /**
  * Respond with the correct character, given the line of the
@@ -27,7 +8,12 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Implement the frontDoorResponse function');
+
+  if (line.length === 0) {
+    return "";
+  } else {
+    return line[0];
+  }
 }
 
 /**
@@ -38,7 +24,7 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Implement the frontDoorPassword function');
+  return capitalize(word);
 }
 
 /**
@@ -49,7 +35,14 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Implement the backDoorResponse function');
+
+  let trimLine = line.trim();
+
+  if (trimLine.length === 0) {
+    return "";
+  } else {
+    return trimLine[trimLine.length - 1];
+  }
 }
 
 /**
@@ -60,5 +53,27 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Implement the backDoorPassword function');
+  return capitalize(word) + ", please";
+}
+
+
+/**
+ * Basic function to capitalize a word.
+ * 
+ * @param {string} word 
+ * @returns {string} The capitalized word
+ */
+function capitalize(word) {
+
+  let newWord = "";
+
+  if (word.length > 0) {
+    newWord += word[0].toUpperCase();
+  }
+
+  if (word.length > 1) {
+    newWord += word.substr(1).toLowerCase();
+  }
+
+  return newWord;
 }
