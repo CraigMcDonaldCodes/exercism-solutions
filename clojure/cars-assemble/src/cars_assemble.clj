a2b9f5358851baa3a57e485aeb4ee13689e84848
Craig McDonald <craig@mcdonald.codes>
2021-12-05 19:41:08 +0000

    Clojure cars assemble completed

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -1,12 +1,19 @@
 (ns cars-assemble)
 
+(def cars-per-hour 221)
+
 (defn production-rate
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
-  )
+  (* speed cars-per-hour
+     (cond
+       (< speed 5) 1.0
+       (< speed 9) 0.9
+       (= speed 9) 0.8
+       :else 0.77)))
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
   [speed]
-  )
+  (int (/ (production-rate speed) 60)))
