6236ac3a643b515e8902273edbde60dee9e7f77e
Craig McDonald <craig@mcdonald.codes>
2022-01-05 00:24:57 +0000

    C difference of squares completed

--- c/difference-of-squares/difference_of_squares.c
+++ c/difference-of-squares/difference_of_squares.c
@@ -1,1 +1,32 @@
 #include "difference_of_squares.h"
+
+#include <math.h>
+#include <stdint.h>
+
+#define u32 uint32_t
+
+u32 sum_of_squares(const u32 number) {
+
+    u32 sum = 0;
+
+    for (u32 i = 1; i <= number; i++) {
+        sum += (u32)pow(i, 2);
+    }
+
+    return sum;
+}
+
+u32 square_of_sum(const u32 number) {
+
+    u32 sum = 0;
+
+    for (u32 i = 1; i <= number; i++) {
+        sum += i;
+    }
+
+    return (u32)pow(sum, 2);
+}
+
+u32 difference_of_squares(const u32 number) {
+    return square_of_sum(number) - sum_of_squares(number);
+}
