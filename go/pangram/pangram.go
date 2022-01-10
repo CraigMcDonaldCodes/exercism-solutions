5a69046c3153d83adcec7a77427d426de7468e08
Craig McDonald <craig@mcdonald.codes>
2022-01-10 22:43:27 +0000

    Go pangram solved

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,5 +1,26 @@
 package pangram
 
+import (
+	"unicode"
+)
+
+// Determine if a string contains all letters of the alphabet
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+
+	set := make(map[rune]bool)
+
+	for _, item := range input {
+
+		item = unicode.ToLower(item)
+
+		if !unicode.IsLetter(item) {
+			continue
+		}
+
+		if _, ok := set[item]; !ok {
+			set[item] = true
+		}
+	}
+
+	return len(set) == 26
 }
