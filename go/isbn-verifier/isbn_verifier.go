f1513ae91dad75ac0a2144f0eff10c3fb7e24111
Craig McDonald <craig@mcdonald.codes>
2022-01-12 23:00:38 +0000

    Go isbn verifier completed

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -1,5 +1,38 @@
 package isbn
 
+import (
+	"strings"
+	"unicode"
+)
+
+const isbnLength = 10
+
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+
+	isbn = strings.ReplaceAll(isbn, "-", "")
+	isbn = strings.ToLower(isbn)
+
+	if len(isbn) != isbnLength {
+		return false
+	}
+
+	sum := 0
+	count := isbnLength
+
+	for _, val := range isbn {
+
+		if unicode.IsNumber(val) {
+			sum += int(val-'0') * count
+		} else if val == 'x' && count == 1 {
+			sum += 10
+		} else {
+			// invalid character, set bad value and return early
+			sum = -1
+			break
+		}
+
+		count--
+	}
+
+	return sum%11 == 0
 }
