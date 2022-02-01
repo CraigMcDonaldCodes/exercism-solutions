7ee61eef8c8536a3996d7e436a448acff278f3fa
Craig McDonald <craig@mcdonald.codes>
2022-02-01 20:34:27 +0000

    Java need for speed completed

--- java/need-for-speed/src/main/java/NeedForSpeed.java
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -1,27 +1,59 @@
 class NeedForSpeed {
-    // TODO: define the constructor for the 'NeedForSpeed' class
+
+    private static final int MIN_BATTERY = 0;
+    private static final int MAX_BATTERY = 100;
+
+    private static final int NITRO_DRAIN = 4;
+    private static final int NITRO_SPEED = 50;
+
+    private int distance;
+    private int speed;
+    private int batteryDrain;
+    private int batteryStatus;
+
+    public NeedForSpeed(int speed, int batteryDrain) {
+
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
+        batteryStatus = MAX_BATTERY;
+    }
 
     public boolean batteryDrained() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.batteryDrained() method");
+        return batteryStatus == MIN_BATTERY;
     }
 
     public int distanceDriven() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.distanceDriven() method");
+        return distance;
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.drive() method");
+
+        if (batteryStatus >= batteryDrain) {
+
+            batteryStatus -= batteryDrain;
+            distance += speed;
+        }
     }
 
     public static NeedForSpeed nitro() {
-        throw new UnsupportedOperationException("Please implement the (static) NeedForSpeed.nitro() method");
+        return new NeedForSpeed(NITRO_SPEED, NITRO_DRAIN);
     }
 }
 
 class RaceTrack {
-    // TODO: define the constructor for the 'RaceTrack' class
+
+    private int distance;
+
+    public RaceTrack(int distance) {
+        this.distance = distance;
+    }
 
     public boolean carCanFinish(NeedForSpeed car) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack.carCanFinish() method");
+
+        while (!car.batteryDrained()) {
+            car.drive();
+        }
+
+        return car.distanceDriven() >= distance;
     }
 }
