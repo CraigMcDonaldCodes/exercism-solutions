070d5aa021dbd7a7f8ff2de05e69116087e0c3d9
Craig McDonald <craig@mcdonald.codes>
2022-02-12 16:46:02 +0000

    JavaScript lasagna master completed

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -1,12 +1,82 @@
 /// <reference path="./global.d.ts" />
 // @ts-check
 
+'use strict';
+
 /**
- * Implement the functions needed to solve the exercise here.
- * Do not forget to export them so they are available for the
- * tests. Here an example of the syntax as reminder:
- *
- * export function yourFunction(...) {
- *   ...
- * }
+ * @param {number} [timeLeft]
  */
+function cookingStatus(timeLeft) {
+
+    switch (timeLeft) {
+        case undefined:
+            return "You forgot to set the timer."
+        case 0:
+            return "Lasagna is done."
+        default:
+            return "Not done, please wait.";
+    }
+}
+
+/**
+ * @param {string[]} layers
+ * @param {number} prepTime
+ */
+function preparationTime(layers, prepTime = 2) {
+
+    // do JS devs test for undefined and null?
+    return layers.length * prepTime;
+}
+
+/**
+ * 
+ * @param {string[]} layers 
+ */
+function quantities(layers) {
+
+    let result = {
+        noodles: 0,
+        sauce: 0,
+    };
+
+    layers.forEach(layer => {
+
+        if (layer === 'noodles') {
+            result.noodles += 50;
+        } else if (layer === 'sauce') {
+            result.sauce += 0.2;
+        }
+    });
+
+    return result;
+}
+
+/**
+ * @param {string[]} friendsList
+ * @param {string[]} myList
+ */
+function addSecretIngredient(friendsList, myList) {
+
+    // assuming there is at least one item in `friendsList`
+    myList.push(friendsList.at(-1));
+}
+
+/**
+ * @param {object} recipe
+ * @param {number} scale
+ */
+function scaleRecipe(recipe, scale) {
+
+    // we don't need a deep copy
+    let newRecipe = {...recipe};
+
+    // determine single value and multiply by scale for each object property
+    // assuming the object passed in is correct
+    for (const key in newRecipe) {
+        newRecipe[key] = (newRecipe[key] / 2) * scale;
+    }
+
+    return newRecipe;
+}
+
+export {cookingStatus, preparationTime, quantities, scaleRecipe, addSecretIngredient}
