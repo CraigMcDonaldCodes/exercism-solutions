9d5bac47a00615f98ecc0d10d21f0faab81bffd7
Craig McDonald <craig@mcdonald.codes>
2022-03-04 23:19:51 +0000

    Go lasagna master completed

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,9 +1,57 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+const noodlesPerLayer = 50
+const saucePerLayer = 0.2
 
-// TODO: define the 'Quantities()' function
+// PreparationTime estimate for the total preparation time based on the number of layers and time per layer
+func PreparationTime(layers []string, time int) int {
 
-// TODO: define the 'AddSecretIngredient()' function
+	if time < 1 {
+		time = 2
+	}
 
-// TODO: define the 'ScaleRecipe()' function
+	return len(layers) * time
+}
+
+// Quantities determine the quantity of noodles and sauce needed
+func Quantities(layers []string) (int, float64) {
+
+	var noodleCount int
+	var sauceCount int
+
+	for _, val := range layers {
+
+		if val == "noodles" {
+			noodleCount++
+		}
+
+		if val == "sauce" {
+			sauceCount++
+		}
+	}
+
+	noodles := noodleCount * noodlesPerLayer
+	sauce := float64(sauceCount) * saucePerLayer
+
+	return noodles, sauce
+}
+
+// AddSecretIngredient
+func AddSecretIngredient(friendsList []string, myList []string) {
+
+	secret := friendsList[len(friendsList)-1]
+	myList[len(myList)-1] = secret
+}
+
+// ScaleRecipe
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+
+	scale := float64(portions) / 2.0
+	newQuantities := make([]float64, len(quantities))
+
+	for idx, _ := range newQuantities {
+		newQuantities[idx] = quantities[idx] * float64(scale)
+	}
+
+	return newQuantities
+}
