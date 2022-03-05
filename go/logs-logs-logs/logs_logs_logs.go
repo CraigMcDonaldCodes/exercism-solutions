ccf79ae5f6acc578eda8865d0d32680d6dc26a49
Craig McDonald <craig@mcdonald.codes>
2022-03-05 23:27:50 +0000

    Go logs logs logs completed

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,48 @@
 package logs
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+
+	appName := "default"
+
+Loop:
+	for _, val := range log {
+
+		switch val {
+		case '❗':
+			appName = "recommendation"
+			break Loop
+		case '🔍':
+			appName = "search"
+			break Loop
+		case '☀':
+			appName = "weather"
+			break Loop
+		}
+	}
+
+	return appName
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+
+	find := string(oldRune)
+	replace := string(newRune)
+
+	newLog := strings.ReplaceAll(log, find, replace)
+
+	return newLog
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return utf8.RuneCount([]byte(log)) <= limit
 }
