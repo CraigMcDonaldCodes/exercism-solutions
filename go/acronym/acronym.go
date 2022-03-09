d1ec830a5f8be65f71a99616d88ae81cc0be9b5e
Craig McDonald <craig@mcdonald.codes>
2022-03-09 23:40:55 +0000

    Go acronym problem solved

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,15 +1,26 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package acronym should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package acronym
 
-// Abbreviate should have a comment documenting it.
+import (
+	"regexp"
+	"strings"
+)
+
+// Abbreviate return an abbreviation of the supplied words
 func Abbreviate(s string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+
+	splitter := regexp.MustCompile(" |-|_")
+
+	words := splitter.Split(s, -1)
+	var sb strings.Builder
+
+	for _, word := range words {
+
+		if word == "" {
+			continue
+		}
+
+		sb.WriteByte(word[0])
+	}
+
+	return strings.ToUpper(sb.String())
 }
