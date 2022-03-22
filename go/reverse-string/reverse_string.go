c7bdf5112efd50f5f838ea6cd195dc9bf30c74cf
Craig McDonald <craig@mcdonald.codes>
2022-03-22 22:23:22 +0000

    Go reverse string completed

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -1,5 +1,21 @@
 package reverse
 
+import "strings"
+
 func Reverse(input string) string {
-	panic("Please implement the Reverse function")
+
+	var sb strings.Builder
+	runes := make([]rune, 0)
+
+	// unwind the unicode string into a slice of runes
+	for _, s := range input {
+		runes = append(runes, s)
+	}
+
+	// build a new string, from the back of the slice
+	for i := len(runes) - 1; i >= 0; i-- {
+		sb.WriteRune(runes[i])
+	}
+
+	return sb.String()
 }
