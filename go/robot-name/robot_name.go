1d53b441b1d7b7e6ca78e7daa76e4e925d8bd866
Craig McDonald <craig@mcdonald.codes>
2022-04-02 13:44:36 +0100

    Go robot names completed

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -1,11 +1,68 @@
 package robotname
 
-// Define the Robot type here.
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
 
-func (r *Robot) Name() (string, error) {
-	panic("Please implement the Name function")
+var robots = make(map[string]bool)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
+func nextName() string {
+
+	// does not take into account running out of names!
+	// max: 26^2 * 10^3
+
+	var sb strings.Builder
+	var name string
+
+	for {
+		// two uppercase
+		sb.WriteRune(rune(rand.Intn(26) + 65))
+		sb.WriteRune(rune(rand.Intn(26) + 65))
+
+		// three numbers
+		sb.WriteRune(rune(rand.Intn(10) + 48))
+		sb.WriteRune(rune(rand.Intn(10) + 48))
+		sb.WriteRune(rune(rand.Intn(10) + 48))
+
+		name = sb.String()
+
+		if _, found := robots[name]; !found {
+
+			robots[name] = true
+			break
+		}
+
+		sb.Reset()
+	}
+
+	return name
+}
+
+// A Robot represents a very simple robot that doesn't do anything
+// other than know its name
+type Robot struct {
+	name string
+}
+
+// Name returns the name of a robot
+func (r *Robot) Name() (string, error) {
+
+	if r.name == "" {
+		r.Reset()
+	}
+
+	// no idea why the return type includes 'error'
+	// all tests passes
+	return r.name, nil
+}
+
+// Reset assigns a new robot name
 func (r *Robot) Reset() {
-	panic("Please implement the Reset function")
+	r.name = nextName()
 }
