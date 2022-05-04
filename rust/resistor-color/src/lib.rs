bd84e2f093ebeec77c6de1f4405f3c654bc789f6
Craig McDonald <craig@mcdonald.codes>
2022-05-04 23:52:56 +0100

    Rust resistor color completed

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -1,28 +1,54 @@
-#[derive(Debug, PartialEq)]
+use std::fmt;
+
+use enum_iterator::IntoEnumIterator;
+use int_enum::IntEnum;
+
+#[repr(usize)]
+#[derive(Clone, Copy, Debug, Eq, IntoEnumIterator, PartialEq, IntEnum)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
 }
 
-pub fn color_to_value(_color: ResistorColor) -> usize {
-    unimplemented!("convert a color into a numerical representation")
+impl fmt::Display for ResistorColor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+pub fn color_to_value(color: ResistorColor) -> usize {
+    color.int_value()
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+
+    let mut resistor = "value out of range".to_string();
+
+    for r in ResistorColor::into_enum_iter() {
+
+        if color_to_value(r) == value {
+            resistor = r.to_string();
+        }
+    }
+
+    resistor
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+
+    let mut colors = Vec::new();
+
+    for r in ResistorColor::into_enum_iter() {
+        colors.push(r)
+    }
+
+    colors
 }
