df11bf63a652a1166c6b011f5c4119d02fe772b8
Craig McDonald <craig@mcdonald.codes>
2022-06-19 16:38:32 +0100

    Go isogram problem solved

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,5 +1,30 @@
 package isogram
 
+import "strings"
+
+// IsIsogram determines if a string is an isogram
+// i.e. no repeating letters
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+
+	runes := make([]bool, 26)
+	result := true
+	word = strings.ToLower(word)
+
+	for _, r := range word {
+
+		if r == ' ' || r == '-' {
+			continue
+		}
+
+		pos := int(r) - 97
+
+		if runes[pos] {
+			result = false
+			break
+		}
+
+		runes[pos] = true
+	}
+
+	return result
 }
