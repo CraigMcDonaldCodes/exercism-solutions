30a7e2bb13ee93087d4b0369dd84aa481ceafcd4
Craig McDonald <craig@mcdonald.codes>
2022-06-21 10:50:50 +0100

    Go flatten-array problem solved

--- go/flatten-array/flatten_array.go
+++ go/flatten-array/flatten_array.go
@@ -1,5 +1,25 @@
 package flatten
 
+// Flatten take an arbitrarily-deep nested slice of any type
+// and returned flattened with any nil values
 func Flatten(nested interface{}) []interface{} {
-	panic("Please implement the Flatten function")
+
+	// empty slice of interfaces
+	flatSlice := []interface{}{}
+
+	if topSlice, ok := nested.([]interface{}); ok {
+
+		for _, entry := range topSlice {
+
+			if nestedSlice, ok := entry.([]interface{}); ok {
+				flatSlice = append(flatSlice, Flatten(nestedSlice)...)
+			} else {
+				if entry != nil {
+					flatSlice = append(flatSlice, entry)
+				}
+			}
+		}
+	}
+
+	return flatSlice
 }
