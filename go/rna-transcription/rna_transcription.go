7f4cdd734c2e422058b781064dbde083a6be2b12
Craig McDonald <craig@mcdonald.codes>
2022-06-21 07:44:46 +0100

    Go rna-transcription problem solved

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,5 +1,25 @@
 package strand
 
+import "strings"
+
+var mapping = map[rune]rune{
+	'G': 'C',
+	'C': 'G',
+	'T': 'A',
+	'A': 'U',
+}
+
+// ToRNA convert a DNA strand into its RNA complement
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+
+	var sb strings.Builder
+
+	for _, r := range dna {
+
+		if val, ok := mapping[r]; ok {
+			sb.WriteRune(val)
+		}
+	}
+
+	return sb.String()
 }
