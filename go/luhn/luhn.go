2d05071d522218a1fd80b23aeaf48702b57f6f25
Craig McDonald <craig@mcdonald.codes>
2022-07-03 16:20:16 +0100

    Go luhn completed

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,5 +1,52 @@
 package luhn
 
+import (
+	"strings"
+)
+
+const (
+	minLength = 2
+	maxValue  = 9
+	asciiZero = 48
+)
+
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+
+	id = strings.ReplaceAll(id, " ", "")
+
+	if len(id) < minLength {
+		return false
+	}
+
+	var (
+		sum        int
+		mustDouble bool
+	)
+
+	for i := len(id) - 1; i >= 0; i-- {
+
+		// convert to what should be 0 - 9 value
+		value := int(id[i]) - asciiZero
+
+		// check value is within range
+		if value < 0 || value > 9 {
+			return false
+		}
+
+		if mustDouble {
+			value *= 2
+
+			if value > maxValue {
+				value -= maxValue
+			}
+
+			mustDouble = false
+		} else {
+			mustDouble = true
+		}
+
+		sum += value
+	}
+
+	return sum%10 == 0
 }
