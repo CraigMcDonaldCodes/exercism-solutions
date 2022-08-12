3a12378e34499ebe459c163235d9f42b439e93cd
Craig McDonald <craig@mcdonald.codes>
2022-08-12 21:22:11 +0100

    Go parsing log files completed

--- go/parsing-log-files/parsing_log_files.go
+++ go/parsing-log-files/parsing_log_files.go
@@ -1,21 +1,60 @@
 package parsinglogfiles
 
+import (
+	"fmt"
+	"regexp"
+)
+
 func IsValidLine(text string) bool {
-	panic("Please implement the IsValidLine function")
+
+	r := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\] .*`)
+
+	return r.MatchString(text)
 }
 
 func SplitLogLine(text string) []string {
-	panic("Please implement the SplitLogLine function")
+
+	r := regexp.MustCompile(`<(~|\*|=|-)*?>`)
+
+	return r.Split(text, -1)
 }
 
 func CountQuotedPasswords(lines []string) int {
-	panic("Please implement the CountQuotedPasswords function")
+
+	r := regexp.MustCompile(`".*?(?i)password.*?"`)
+	var count int
+
+	for _, line := range lines {
+		// feels a bit ugly
+		count += len(r.FindAllStringIndex(line, -1))
+	}
+
+	return count
 }
 
 func RemoveEndOfLineText(text string) string {
-	panic("Please implement the RemoveEndOfLineText function")
+
+	// problem does not want to remove resulting extra whitespace
+	r := regexp.MustCompile(`end-of-line\d+`)
+
+	return r.ReplaceAllString(text, "")
 }
 
 func TagWithUserName(lines []string) []string {
-	panic("Please implement the TagWithUserName function")
+
+	tagLines := make([]string, 0, len(lines))
+	r := regexp.MustCompile(`User +(\w+\d+)`)
+
+	for _, line := range lines {
+
+		matches := r.FindStringSubmatch(line)
+
+		if len(matches) == 2 {
+			line = fmt.Sprintf("[USR] %s %s", matches[1], line)
+		}
+
+		tagLines = append(tagLines, line)
+	}
+
+	return tagLines
 }
