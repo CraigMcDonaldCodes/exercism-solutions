cdfcd2754958a6e82eb49e1d65e7672c6a7c5d60
Craig McDonald <craig@mcdonald.codes>
2022-08-13 11:53:46 +0100

    Go armstrong numbers solved

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,21 @@
 package armstrong
 
+import (
+	"math"
+	"strconv"
+)
+
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+
+	power := float64(len(strconv.Itoa(n)))
+	current := n
+	var sum float64
+
+	for current > 0 {
+		t := float64(current % 10)
+		sum += math.Pow(t, power)
+		current /= 10
+	}
+
+	return float64(n) == sum
 }
