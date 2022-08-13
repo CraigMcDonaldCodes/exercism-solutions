a3c1000fd1997e5b4d2de4887c774192a44f5235
Craig McDonald <craig@mcdonald.codes>
2022-08-13 12:17:39 +0100

    Rust armstrong numbers completed

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,13 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+
+    let mut sum = 0u32;
+    let mut current = num;
+    let power = num.to_string().len() as u32;
+
+    while current > 0 {
+        sum += u32::pow(current % 10, power);
+        current /= 10;
+    }
+
+    num == sum
 }
