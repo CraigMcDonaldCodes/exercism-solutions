e5fa011d2ba756a608b772eb05331cf21e6a34d4
Craig McDonald <craig@mcdonald.codes>
2022-08-27 17:48:58 +0100

    Rust matching brackets problem solved

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,6 +1,29 @@
+use std::collections::HashMap;
+
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+
+    let closing_char: HashMap<char, char> = HashMap::from([
+        (')','('),
+        (']','['),
+        ('}','{'),
+    ]);
+
+    let mut stack = Vec::new();
+
+    for c in string.chars() {
+
+        match c {
+            '(' | '[' | '{' => stack.push(c),
+            ')' | ']' | '}' => {
+                if stack.len() > 0 && (stack.last() == closing_char.get(&c)) {
+                    stack.pop();
+                } else {
+                    return false
+                }
+            },
+            _ => (),
+        }
+    }
+
+    stack.len() == 0
 }
