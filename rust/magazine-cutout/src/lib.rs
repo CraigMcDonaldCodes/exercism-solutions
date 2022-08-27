d56370481902246e54aed5a6dada577f51c8f12a
Craig McDonald <craig@mcdonald.codes>
2022-08-27 17:25:06 +0100

    Rust magazine cutout completed

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -1,9 +1,26 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
+// this is probably not idiomatic Rust, still learning
 
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+
+    let mut required: HashMap<&str, u32> = HashMap::new();
+
+    for &n in note {
+        required.entry(n)
+                .and_modify(|e| *e += 1)
+                .or_insert(1);
+    }
+
+    for &m in magazine {
+        if required.contains_key(m) {
+            if required[m] == 1 {
+                required.remove(m);
+            } else {
+                required.entry(m).and_modify(|e| *e -= 1);
+            }
+        }
+    }
+
+    required.keys().len() == 0
 }
