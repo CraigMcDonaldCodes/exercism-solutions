a1ae6bff2a05b738af514fb2d06e0f756f88f040
Craig McDonald <craig@mcdonald.codes>
2023-01-14 22:02:41 +0000

    JavaScript lucky numbers solved

--- javascript/lucky-numbers/lucky-numbers.js
+++ javascript/lucky-numbers/lucky-numbers.js
@@ -8,7 +8,21 @@
  * @returns {number} sum of the two arrays
  */
 export function twoSum(array1, array2) {
-  throw new Error('Implement the twoSum function');
+
+  let sum1 = 0; 
+  let sum2 = 0; 
+
+  for (let i = 0; i < array1.length; i++) {
+    sum1 *= 10;
+    sum1 += array1[i];
+  }
+
+  for (let i = 0; i < array2.length; i++) {
+    sum2 *= 10;
+    sum2 += array2[i];
+  }
+
+  return sum1 + sum2;
 }
 
 /**
@@ -18,7 +32,17 @@ export function twoSum(array1, array2) {
  * @returns {boolean}  whether the number is a palindrome or not
  */
 export function luckyNumber(value) {
-  throw new Error('Implement the luckyNumber function');
+
+  let tmp = value;
+  let revValue = 0;
+
+  while (tmp > 0) {
+    revValue *= 10;
+    revValue += (tmp % 10);
+    tmp = Math.floor(tmp/10); // no int division by default
+  }
+
+  return value === revValue;
 }
 
 /**
@@ -29,5 +53,15 @@ export function luckyNumber(value) {
  * @returns {string} error message
  */
 export function errorMessage(input) {
-  throw new Error('Implement the errorMessage function');
+
+  if (input === null || input === undefined || input.length === 0) {
+    return "Required field";
+  }
+
+  let num = Number(input);
+  if (Number.isNaN(num) || num === 0) {
+    return "Must be a number besides 0";
+  }
+
+  return "";
 }
