2afa54daf5d04d6a310531d00e2cfbed51ccfbaf
Craig McDonald <craig@mcdonald.codes>
2023-03-06 21:29:07 +0000

    Python 'little sisters vocab' solved

--- python/little-sisters-vocab/strings.py
+++ python/little-sisters-vocab/strings.py
@@ -1,54 +1,80 @@
+"""Functions for creating, transforming, and adding prefixes to strings."""
+
+
 def add_prefix_un(word):
+    """Take the given word and add the 'un' prefix.
+
+    :param word: str - containing the root word.
+    :return: str - of root word prepended with 'un'.
     """
 
-    :param word: str of a root word
-    :return:  str of root word with un prefix
-
-    This function takes `word` as a parameter and
-    returns a new word with an 'un' prefix.
-    """
-
-    pass
+    return f"un{word}"
 
 
 def make_word_groups(vocab_words):
-    """
+    """Transform a list containing a prefix and words into a string.
 
-    :param vocab_words: list of vocabulary words with a prefix.
-    :return: str of prefix followed by vocabulary words with
-             prefix applied, separated by ' :: '.
+    :param vocab_words: list - of vocabulary words with prefix in first index.
+    :return: str - of prefix followed by vocabulary words with
+            prefix applied.
 
     This function takes a `vocab_words` list and returns a string
-    with the prefix  and the words with prefix applied, separated
+    with the prefix and the words with prefix applied, separated
      by ' :: '.
+
+    For example: list('en', 'close', 'joy', 'lighten'),
+    produces the following string: 'en :: enclose :: enjoy :: enlighten'.
     """
 
-    pass
+    length = len(vocab_words)
+    answer: str = ""
+
+    if length == 1:
+        answer = vocab_words[0]
+    elif length > 1:
+        prefix = vocab_words[0]
+        results = []
+        results.append(prefix)
+
+        for word in vocab_words[1:]:
+            results.append(prefix + word)
+
+        answer = " :: ".join(results)
+
+    return answer
 
 
 def remove_suffix_ness(word):
+    """Remove the suffix from the word while keeping spelling in mind.
+
+    :param word: str - of word to remove suffix from.
+    :return: str - of word with suffix removed & spelling adjusted.
+
+    For example: "heaviness" becomes "heavy", but "sadness" becomes "sad".
     """
 
-    :param word: str of word to remove suffix from.
-    :return: str of word with suffix removed & spelling adjusted.
+    answer: str = word
 
-    This function takes in a word and returns the base word with `ness` removed.
+    if word.endswith("ness"):
+        if word[-5] == "i":
+            answer = word[:-5]
+            answer = answer + "y"
+        else:
+            answer = word[:-4]
+
+    return answer
+
+
+def adjective_to_verb(sentence, index):
+    """Change the adjective within the sentence to a verb.
+
+    :param sentence: str - that uses the word in sentence.
+    :param index: int - index of the word to remove and transform.
+    :return: str - word that changes the extracted adjective to a verb.
+
+    For example, ("It got dark as the sun set", 2) becomes "darken".
     """
 
-    pass
-
-
-def noun_to_verb(sentence, index):
-    """
-
-    :param sentence: str that uses the word in sentence
-    :param index:  index of the word to remove and transform
-    :return:  str word that changes the extracted adjective to a verb.
-
-    A function takes a `sentence` using the
-    vocabulary word, and the `index` of the word once that sentence
-    is split apart.  The function should return the extracted
-    adjective as a verb.
-    """
-
-    pass
+    # assuming the index supplied is valid for the string
+    # and it's a sentence ending in a full stop
+    return f'{sentence[:-1].split(" ")[index]}en'
