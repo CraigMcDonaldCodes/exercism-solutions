5bb46d8e061f03bcabeb693588057379266c3d72
Craig McDonald <craig@mcdonald.codes>
2023-04-09 12:11:29 +0100

    Go difference of squares completed

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,1 +1,32 @@
 package diffsquares
+
+import "math"
+
+func SquareOfSum(max int) int {
+
+	var sum int
+
+	for i := 1; i <= max; i++ {
+		sum += i
+	}
+
+	return powTwoInt(sum)
+}
+
+func SumOfSquares(max int) int {
+	var sum int
+
+	for i := 1; i <= max; i++ {
+		sum += powTwoInt(i)
+	}
+
+	return sum
+}
+
+func Difference(max int) int {
+	return SquareOfSum(max) - SumOfSquares(max)
+}
+
+func powTwoInt(n int) int {
+	return int(math.Pow(float64(n), 2))
+}
