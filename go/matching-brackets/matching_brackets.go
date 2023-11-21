d9a2b56bea51d3b61e3b9808ca1a2e6b41eb96f6
Craig McDonald <craig@mcdonald.codes>
2023-11-21 20:58:05 +0000

    Go matching brackets completed

--- go/matching-brackets/matching_brackets.go
+++ go/matching-brackets/matching_brackets.go
@@ -1,5 +1,32 @@
 package brackets
 
+var closingRune = map[rune]rune{
+	']': '[',
+	'}': '{',
+	')': '(',
+}
+
 func Bracket(input string) bool {
-	panic("Please implement the Bracket function")
+
+	// no built in 'stack' data structure ☹️
+	// so we half-arse it
+	var stack []rune
+
+	for _, r := range input {
+		switch r {
+
+		case '[', '{', '(':
+			stack = append(stack, r)
+		case ']', '}', ')':
+			top := len(stack) - 1
+
+			if len(stack) > 0 && (stack[top] == closingRune[r]) {
+				stack = stack[:top]
+			} else {
+				return false
+			}
+		}
+	}
+
+	return len(stack) == 0
 }
